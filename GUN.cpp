85d309956bce4a7950ee6fa8d6981da6a5ea7376
Hamdanaleem <144408426+Hamdanaleem@users.noreply.github.com>
2023-09-16 15:47:11 +0500

    Add files via upload

---
+++ GUN.cpp
@@ -0,0 +1,28 @@
+#inlude<iostream>
+using namespace std;
+main(){
+	system("color 09")
+
+	cout<<"     +--^----------,--------,-----,--------^-,            "<<endl;
+	cout<<"      | |||||||||   '--------'     |          o           "<<endl;
+	cout<<"      '+---------------------------^----------|           "<<endl;
+	cout<<"        ',---------,---------,--------------'             "<<endl;
+	cout<<"          / XXXXXX /'|       /'                           "<<endl;
+	cout<<"         / XXXXXX /  |      /'                            "<<endl;
+	cout<<"        / XXXXXX /'-------'                               "<<endl;
+	cout<<"       / XXXXXX /                                         "<<endl;
+	cout<<"      / XXXXXX /                                          "<<endl;
+	cout<<"      (--------(                                          "<<endl;
+	cout<<"       '------'                                           "<<endl;
+	
+
+
+
+
+
+
+
+
+
+
+}
